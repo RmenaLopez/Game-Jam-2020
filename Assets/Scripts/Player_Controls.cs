770c21428791efa1becaa4d29c59131624c99dcb
AlekNiebla <alek.nibo@gmail.com>
2021-01-30 22:57:50 -0700

    push por el mena

---
+++ Assets/Scripts/Player_Controls.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controls : MonoBehaviour
+{
+    [SerializeField]
+    private float move_speed = 2f;
+
+    private Rigidbody2D rigibody2D;
+    private Vector3 moveDir;
+
+    private void Awake()
+    {
+        rigibody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = 0f;
+        float moveY = 0f;
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveY = +1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveY = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX = +1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveX = -1f;
+        }
+
+        moveDir = new Vector3(moveX, moveY).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rigibody2D.velocity = moveDir * move_speed;
+    }
+
+    public void SetMoveSpeed(float score)
+    {
+        float percentage = (1 * score) / 50;
+        float acceleration = Mathf.Lerp(0, 8, percentage);
+        this.move_speed = 2f + acceleration;
+    }
+}
---
+++ Assets/Scripts/Score_Manager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Score_Manager : MonoBehaviour
+{
+    private float score = 0;
+    Light_Control lightControls;
+    Player_Controls playerControls;
+    // Update is called once per frame
+
+    private void Start()
+    {
+        lightControls = GameObject.FindGameObjectWithTag("PlayerLight").GetComponent<Light_Control>();
+        playerControls = GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Controls>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (this.score != 50)
+            {
+                this.score += 1;
+            }
+            lightControls.SetRadiusRange(this.score);
+            playerControls.SetMoveSpeed(this.score);
+            Debug.Log("SCORE: " + this.score);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (this.score != 0)
+            {
+                this.score -= 1;
+            }
+            lightControls.SetRadiusRange(this.score);
+            playerControls.SetMoveSpeed(this.score);
+            Debug.Log("SCORE: " + this.score);
+        }
+
+    }
+}
