2bcc4a8547b56da86623fac8bfaad2fac8726e4a
rafael mena <rafamenalopez@gmail.com>
2021-01-30 19:34:45 -0800

    initial commit mena

---
+++ Assets/Scripts/Path/PathManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class PathManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform start;
+    [SerializeField]
+    private Transform end;
+    [SerializeField]
+    private Transform pathLight;
+    // Start is called before the first frame update
+
+    private Rigidbody2D endRb;
+    private Rigidbody2D startRb;
+
+    private int currentWaypoint = 0;
+    private bool reachEndOfPath = false;
+
+    Seeker seeker;
+    Path path;
+    
+
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        endRb = end.GetComponent<Rigidbody2D>();
+        startRb = start.GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, 2.5f);
+        
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+            Debug.Log("waypoints = " + path.vectorPath.Count);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (path == null)
+            return;
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            reachEndOfPath = true;
+            return;
+        }else 
+        {
+            reachEndOfPath = false;
+        }
+
+        if (!reachEndOfPath)
+        {
+            GameObject.Instantiate(pathLight, path.vectorPath[currentWaypoint], Quaternion.identity);
+            currentWaypoint += 2;
+        }
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+            seeker.StartPath(startRb.position, endRb.position, OnPathComplete);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Collider2D innerCircle;
+    [SerializeField]
+    private GameObject outterCircle;
+
+    private float spawnTime = 60f / 100f;
+    [SerializeField]
+    private List<GameObject> outterCircleList;
+
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        InvokeRepeating("SpawnBeat", 0, spawnTime * 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("space"))
+        {
+            if (IsHitAttemptSuccessful())
+            {
+                gameManager.GainPoints();
+            }
+            else
+            {
+                gameManager.LosePoints();
+            }
+        }
+    }
+
+
+    private void SpawnBeat()
+    {
+        CheckOutterCircleList();
+        GameObject newOutterCircle = Instantiate(outterCircle, new Vector3(0, 0, 0), Quaternion.identity);
+        newOutterCircle.transform.parent = gameObject.transform;
+        outterCircleList.Add(newOutterCircle);
+    }
+
+    private void CheckOutterCircleList()
+    {
+        if (outterCircleList.Count > 0)
+        {
+            if (!outterCircleList[0])
+            {
+                outterCircleList.RemoveAt(0);
+            }
+        }
+    }
+
+    private bool IsHitAttemptSuccessful()
+    {
+        CheckOutterCircleList();
+        if (outterCircleList.Count > 0)
+        {
+            bool res = outterCircleList[0].GetComponent<OutterCircleController>().IsInputSuccessful();
+            Destroy(outterCircleList[0].gameObject);
+            return res;
+        }
+        return false;
+    }    
+}
